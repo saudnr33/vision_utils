daaea7392b80b2615c179fc0e572d1d2ea0658d1
Saud  Alrasheed <saudnr33@gmail.com>
2023-10-02 12:00:51 -0700

    temp comm

--- data_exp_tbd.py
+++ data_exp_tbd.py
@@ -3,23 +3,37 @@ import cv2
 import glob
 import numpy as np
 import utils.plot_utils
-
+from tqdm import tqdm
 
 if __name__ == "__main__":
     #test for dev
-    wcam_sd_dir = "/media/basic_data/ht_datasets_ml_datasets_wcam_sd_v1.5/wcam_sd"
-    file_list  = os.listdir(wcam_sd_dir)
+    wcam_sd_dir = "/media/basic_data/saud_sd"
+    kps_2d_files = [f for f in sorted(os.listdir("/media/basic_data/saud_sd")) \
+                    if "_kps_2d.npy" in f]
 
-    kps_vis = [f for f in file_list if "kps_vis" in f]
+    for f in tqdm(kps_2d_files):
+        data_point = f.split("_")[0]
+        kps_2d = np.load(os.path.join(wcam_sd_dir, f))
+        source = np.zeros( (288, 288, 3))
+        source = utils.plot_utils.draw_kps2d_img(source, kps_2d, add_text=False)
+        source = cv2.resize(source, (256, 256))    
+        cv2.imwrite(os.path.join(wcam_sd_dir, data_point + "_control.png"), source)
+
+
+    # file_list  = os.listdir(wcam_sd_dir)
+
+    # kps_vis = [f for f in file_list if "kps_vis" in f]
     
 
-    index = 0
-    data_point =   kps_vis[index].split("_")[0]
-    kps_vis_0 = np.load(os.path.join(wcam_sd_dir, kps_vis[index]))
-    kps_2d = np.load(os.path.join(wcam_sd_dir, f'{data_point}_kps_2d.npy'))
-    im = cv2.imread(os.path.join(wcam_sd_dir, f'{data_point}_0_generated.png'))
+    # index = 0
+    # data_point =   kps_vis[index].split("_")[0]
+    # kps_vis_0 = np.load(os.path.join(wcam_sd_dir, kps_vis[index]))
+    # kps_2d = np.load(os.path.join(wcam_sd_dir, f'{data_point}_kps_2d.npy'))
+    # kps_3d = np.load(os.path.join(wcam_sd_dir, f'{data_point}_kps_3d.npy'))
 
-    print(im.shape)
-    im = utils.plot_utils.draw_kps2d(im, kps_2d, show_indices=True)
-    cv2.imwrite("samples/test.png", im)
-    # print(data_point,kps_2d, kps_vis_0)
+    # im = cv2.imread(os.path.join(wcam_sd_dir, f'{data_point}_0_generated.png'))
+
+    # print(im.shape, kps_vis_0.shape, kps_2d.shape, kps_3d.shape)
+    # im = utils.plot_utils.draw_kps2d(im, kps_2d, show_indices=True)
+    # cv2.imwrite("samples/test.png", im)
+    # # print(data_point,kps_2d, kps_vis_0)
--- utils/plot_utils.py
+++ utils/plot_utils.py
@@ -26,6 +26,94 @@ def draw_kps2d(image, kps2d, show_indices = False):
                                            cv2.LINE_AA)
     return image
 
+def draw_finger_line(image, kps2d, colors):
+    core = kps2d[-1]
+    for i in range(5):
+        for j in range(3):
+            x1, y1 = kps2d[4*i + j]
+            x2, y2 = kps2d[4*i + j + 1]
+            if not (np.isnan(x1) or np.isnan(x2)):
+                start_point = (round(x1), round(y1) )
+                end_point = (round(x2), round(y2))
+                # Green color in BGR
+                color = colors[4*i + j]
+                # Line thickness of 9 px
+                thickness = 1
+
+                image = cv2.line(image,
+                                  start_point,
+                                    end_point,
+                                      color,
+                                        thickness)
+        
+        if not (np.isnan(core[0]) or np.isnan(x2)):
+                start_point = (round(core[0]), round(core[1]))
+                end_point = (round(x2), round(y2))
+                color = colors[4*i + 3]
+                thickness = 2
+                image = cv2.line(image,
+                                  start_point,
+                                    end_point,
+                                      color,
+                                        thickness)
+    return image
+
+
+def draw_kps2d_img(image, kps2d, add_text = False):
+    '''
+    image: cv2 image (or array like cv2 image)
+    kps2s: array of shape [n_kps, 2]
+    '''
+    colors = [[250, 110, 110],
+                [242, 103, 113],
+                [233, 97, 117],
+                [224, 92, 119],
+                [214, 87, 121],
+                [204, 83, 123],
+                [193, 79, 124],
+                [182, 76, 125],
+                [171, 73, 125],
+                [159, 71, 125],
+                [147, 68, 124],
+                [135, 66, 122],
+                [123, 64, 120],
+                [111, 61, 117],
+                [99, 59, 114],
+                [87, 56, 109],
+                [76, 54, 105],
+                [64, 51, 100],
+                [53, 48, 94],
+                [42, 45, 88]]
+    core = (255, 126, 0)
+    for i, (x,y) in enumerate(kps2d):
+        if not np.isnan(x):
+            if i ==20:
+                color = core
+            else:
+                color = colors[i] 
+            image = cv2.circle(image, 
+                            (round(x), round(y)),
+                                3,
+                                color,
+                                    4)
+            
+            if add_text:
+                org = (round(x), round(y))
+                fontScale = 0.5
+                thickness = 1
+                font = cv2.FONT_HERSHEY_SIMPLEX
+                color = (255, 0, 0)
+                image = cv2.putText(image,
+                                     f'{i}',
+                                       org, 
+                                       font, 
+                                       fontScale,
+                                         color,
+                                           thickness,
+                                             cv2.LINE_AA)  
+    image = draw_finger_line(image, kps2d, colors)           
+    return image
+
 
 def image_overlayer(im_source, im_overlay, remove_black_background = False):
     '''
